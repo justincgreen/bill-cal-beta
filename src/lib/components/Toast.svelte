a06fffed070eecc11de9383b6de130a5e33dff02
Justin Green <justingreendev@gmail.com>
2024-08-08 09:56:25 -0500

    styling

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -6,3 +6,13 @@
 <div class="c-toast">
   { $toastValue }
 </div>
+
+<style lang="scss">
+  .c-toast {
+    display: inline-flex;
+    border-radius: 5px;
+    padding: 20px;
+    background-color: lightgreen;
+    
+  }
+</style>
