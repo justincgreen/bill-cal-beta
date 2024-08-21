feea5a19e7bed6b0ddadce9a60fed6945023d77c
Justin Green <justingreendev@gmail.com>
2024-08-21 15:22:51 -0500

    Update button

--- src/lib/components/BillForm.svelte
+++ src/lib/components/BillForm.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
-  
+  const handleBill = () => {
+    alert();
+  }
 </script>
 
 <div class="c-bill-form">
   <h3>Bill Form</h3>
-  <input type="date">
-  <input type="text" placeholder="Bill Title">
-  <input type="text" placeholder="Bill Amount">
-  <button>Create Bill</button>
+  <div class="c-bill-form__wrapper">
+    <input type="date">    
+    <input type="text" placeholder="Bill Title">
+    <input type="text" placeholder="Bill Amount">  
+    <button on:click={handleBill}>Create Bill</button>
+  </div>
 </div>
 
 <style lang="scss">
   .c-bill-form {
     background-color: coral;
-    padding: 5px;    
+    padding: 5px;  
+    
+    .c-bill-form__wrapper {
+      padding: 10px;
+      
+      input {
+        display: block;
+        width: 100%;        
+        padding: 10px;
+        margin-bottom: 20px;
+        border-radius: 5px;
+        border: none;
+        box-sizing: border-box;
+      }            
+    }  
+    
+    button {
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
   }
 </style>
